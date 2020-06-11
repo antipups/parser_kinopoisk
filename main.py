54aa1bde2a0b4fc37d453652d1ea4304607cff1e
antipups <nick.kurkurin@protonmail.com>
2020-06-11 12:58:06 +0300

    Сделан занос в БД, с ссылкой на картинку, осталось проверить и перенастроить общйи парс.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import re
 import requests
 
+import util
+
 
 def get_page_with_films(number_page):
     headers = {'cookie': 'navigator_settings=%5B%7B%22rating%22%3A%224%3A%22%7D%2C%22rating%22%5D; mda_exp_enabled=1; _ym_uid=1579529704866061345; mda=0; yandexuid=4775578911579527928; yuidss=4775578911579527928; yandex_login=nick.kurkurin; i=9i3UxfDBmCNL75FEZx4U3m5mvyntQ3SOrXWOe2GcwT9RUFwvONHEehBobaOYt8qcqsCkDxk5AOlFzVem9dcmXnILw7w=; mustsee_sort_v5=01.10.200.21.31.41.121.131.51.61.71.81.91.101.111; lfiltr=all; gdpr=0; __gads=ID=6efe48584a968282:T=1585435663:S=ALNI_MYTKh2uwZR3uJ92wdW6gcL8-agexw; yandex_ugc_modal_dialog=apply; vote_data_cookie=88a70c35167db0a8a6bd50f25936be00; fuid01=5e2c9b512f50800a.XKmg3uxApTtabYZ4CzrUZBvyIO2xRfbMMk522nR7m2BuaU-1z3cKyL1cRG-p_cLskmJIPpgAe5zAsoEi-ZXST4HeSywzVP9WIfMirel3aRia4kHnnlzLJVywKy_C0qBV; PHPSESSID=dk540f4hmu80mg3kc92agkgg91; yandex_gid=142; tc=370; uid=18669245; _csrf_csrf_token=33HPuPKQk48L5tEp4wkHRn8raahH2u039ukReeM22Ow; mobile=no; desktop_session_key=6a2c2ac7756fa9d4fd17f6f21a00f7319b15797226077f8fdf2f9c58db9fad7859fd329375e7967ec99a4b75c63a4858fa3bda88a5e5c43d9653cf126cf9d19bab169e4ef1c1332198ea1595c07d965c32cb3faf072893366bdeef4afac2adc8; desktop_session_key.sig=fR3aoymlc781RG3dpJw0hGN8sDU; _ym_wasSynced=%7B%22time%22%3A1591711356960%2C%22params%22%3A%7B%22eu%22%3A0%7D%2C%22bkParams%22%3A%7B%7D%7D; _ym_isad=2; yp=1584129332.oyu.4775578911579527928#1591797760.yu.4775578911579527928; ymex=1594303360.oyu.4775578911579527928; user-geo-region-id=142; user-geo-country-id=62; user_country=ua; _ym_visorc_22663942=b; _ym_visorc_52332406=b; _ym_visorc_56177992=b; _ym_visorc_26812653=b; ya_sess_id=3:1591722387.5.0.1579529751482:5O7H2Q:2.1|230517873.0.2|30:190171.230791.tJ1qMMOgQcJCYtveYf7HwNx3ZaE; ys=udn.cDpuaWNrLmt1cmt1cmlu#c_chck.2170997977; mda2_beacon=1591722387940; sso_status=sso.passport.yandex.ru:synchronized; _ym_visorc_238735=w; _ym_visorc_237742=w; _ym_visorc_238724=w; yandex_plus_metrika_cookie=true; _ym_visorc_10630330=b; spravka=dD0xNTkxNzIzNDUwO2k9MjE3LjE5OS4yMzkuMTEwO3U9MTU5MTcyMzQ1MDcwNjQwMjQ5NjtoPTVjZWI2ZjQ5YWMzZGRmZDJiN2RmOTAzYWNlNDJiMjdk; my_perpages=%5B%5D; cycada=i+LY98SA4Su4axF/7c/ydPz2/9NbechaydrerABCsV4=; _ym_d=1591723539',
@@ -17,11 +19,6 @@ def get_page_with_films(number_page):
 
 
 def get_title(html_code):
-    """
-        Функция на получение названия, для сериалов один шаблон для фильмов другой
-    :param html_code:
-    :return:
-    """
     if html_code.find('"@context"') > -1:
         return re.search(r'name":"((?!\",\").)*', html_code).group()[7:]
     else:
@@ -29,19 +26,85 @@ def get_title(html_code):
 
 
 def func_for_clear(data):
+    """
+        Функция для очистки строки от спец. символов и тегов.
+    :param data:
+    :return: чистая строка
+    """
     return re.sub(r'<[^>]*>|&[^;]*;',  ' ', data)
 
 
 def get_description(html_code):
     if html_code.find('"@context"') > -1:
-        return re.search(r'description":"((?!\",\").)*', html_code).group()[-14:]
+        return re.search(r'description":"((?!\",\").)*', html_code).group()[14:]
     else:
-        if description := re.search(r'itemprop="description">((?!<\/div>).)*', html_code):
+        if description := re.search(r'itemprop="description">((?!<\/div>).)*', html_code):      # иногда описания может и не быть
             return func_for_clear(description.group()[23:])
         else:
             return ''
 
+
+def get_genres(html_code):
+    if html_code.find('"@context"') > -1:
+        return eval(re.search(r'genre":[^]]*', html_code).group()[7:] + ']')
+    else:
+        html_code = html_code[html_code.find('itemprop="genre">') + 17:]
+        html_code = html_code[:html_code.find('</span>')]
+        list_of_genres = []
+        for genre in re.finditer(r'>[^,]((?!<\/a>).)*', html_code):
+            list_of_genres.append(genre.group()[1:])
+        return list_of_genres
+
+
+def get_year(html_code):
+    if html_code.find('"@context"') > -1:
+        return re.search(r'mC3xgnbJn9r8KpG71O4nw _2PpOsV4r_uZ1kfCMVw4YLb[^?]*', html_code).group()[-5:-1]
+    else:
+        return re.search(r'год.*>\d{4}<\/a', html_code, flags=re.DOTALL).group()[-7:-3]
+
+
+def get_rating(html_code):
+    if html_code.find('"@context"') > -1:
+        return re.search(r'ratingValue":[^,]*', html_code).group()[13:]
+    else:
+        return re.search(r'rating:[^,]*', html_code).group()[7:]
+
+
+def get_director(html_code):
+    list_of_directors = []
+    if html_code.find('"@context"') > -1:
+        for director in eval(re.search(r'director":[^]]*', html_code).group()[10:] + ']'):
+            list_of_directors.append(director.get('name'))
+    else:
+        incorrect_directors = re.search('itemprop="director">((?!<\/tr).)*', html_code).group()[21:]
+        for directors in re.findall(r'>[^,.][^<]*<', incorrect_directors):
+            list_of_directors.append(directors[1:-1])
+
+    return list_of_directors
+
+
+def get_slug(html_code):
+    if html_code.find('"@context"') > -1:
+        if slug := re.search(r'_3ja1RtuInxvxsgjHtSWGjM _1nml1ld3eOhePX5JvGVDW7\">«[^»]*', html_code):
+            return slug.group()[50:]
+    else:
+        if slug := re.search(r'слоган<\/td>((?!\/tr).)*', html_code):
+            slug = slug.group()
+            if slug.find('>-<') == -1:
+                return slug[slug.find('&laquo;') + 7: slug.find('&raquo;')]
+    return ''
+
+
+def get_poster(html_code):
+    if html_code.find('"@context"') > -1:
+        poster = html_code[html_code.find('film-poster _1Vb4pWX9XKqLB1ARq_tTbD image-partial-component _744Do9HoKgS_juORQz12m _3ImO1wdHPSkNycqGSMjFWL'):]
+        return 'https:' + re.search(r'src=\"[^\"]*', poster).group()[5:]
+    else:
+        return re.search(r'property=\"og:image\" content=\"[^\"]*', html_code).group()[29:]
+
+
 def read_one_film(film_url):
+    print('Начинаю загружать - ', film_url[5:-1])
     headers = {'accept-language': 'ru;q=0.8',
                'cookie': 'mda_exp_enabled=1; _ym_uid=1579529704866061345; mda=0; yandexuid=4775578911579527928; yuidss=4775578911579527928; yandex_login=nick.kurkurin; i=9i3UxfDBmCNL75FEZx4U3m5mvyntQ3SOrXWOe2GcwT9RUFwvONHEehBobaOYt8qcqsCkDxk5AOlFzVem9dcmXnILw7w=; mustsee_sort_v5=01.10.200.21.31.41.121.131.51.61.71.81.91.101.111; lfiltr=all; gdpr=0; __gads=ID=6efe48584a968282:T=1585435663:S=ALNI_MYTKh2uwZR3uJ92wdW6gcL8-agexw; yandex_ugc_modal_dialog=apply; vote_data_cookie=88a70c35167db0a8a6bd50f25936be00; fuid01=5e2c9b512f50800a.XKmg3uxApTtabYZ4CzrUZBvyIO2xRfbMMk522nR7m2BuaU-1z3cKyL1cRG-p_cLskmJIPpgAe5zAsoEi-ZXST4HeSywzVP9WIfMirel3aRia4kHnnlzLJVywKy_C0qBV; PHPSESSID=dk540f4hmu80mg3kc92agkgg91; yandex_gid=142; tc=370; uid=18669245; _csrf_csrf_token=33HPuPKQk48L5tEp4wkHRn8raahH2u039ukReeM22Ow; mobile=no; desktop_session_key=6a2c2ac7756fa9d4fd17f6f21a00f7319b15797226077f8fdf2f9c58db9fad7859fd329375e7967ec99a4b75c63a4858fa3bda88a5e5c43d9653cf126cf9d19bab169e4ef1c1332198ea1595c07d965c32cb3faf072893366bdeef4afac2adc8; desktop_session_key.sig=fR3aoymlc781RG3dpJw0hGN8sDU; _ym_wasSynced=%7B%22time%22%3A1591711356960%2C%22params%22%3A%7B%22eu%22%3A0%7D%2C%22bkParams%22%3A%7B%7D%7D; _ym_isad=2; yp=1584129332.oyu.4775578911579527928#1591797760.yu.4775578911579527928; ymex=1594303360.oyu.4775578911579527928; user-geo-region-id=142; user-geo-country-id=62; user_country=ua; _ym_visorc_22663942=b; _ym_visorc_52332406=b; _ym_visorc_56177992=b; _ym_visorc_26812653=b; ya_sess_id=3:1591722387.5.0.1579529751482:5O7H2Q:2.1|230517873.0.2|30:190171.230791.tJ1qMMOgQcJCYtveYf7HwNx3ZaE; ys=udn.cDpuaWNrLmt1cmt1cmlu#c_chck.2170997977; mda2_beacon=1591722387940; sso_status=sso.passport.yandex.ru:synchronized; _ym_visorc_238735=w; _ym_visorc_237742=w; _ym_visorc_238724=w; spravka=dD0xNTkxNzIzNDUwO2k9MjE3LjE5OS4yMzkuMTEwO3U9MTU5MTcyMzQ1MDcwNjQwMjQ5NjtoPTVjZWI2ZjQ5YWMzZGRmZDJiN2RmOTAzYWNlNDJiMjdk; mykp_button=group; my_perpages=%7B%2248%22%3A200%7D; _ym_visorc_238726=w; yandex_plus_metrika_cookie=true; _ym_d=1591728484; cycada=nCFgZm2DjNP1qOpZfi3Ikvz2/9NbechaydrerABCsV4=',
                "sec-fetch-dest": "document",
@@ -51,18 +114,32 @@ def read_one_film(film_url):
                "upgrade-insecure-requests": "1",
                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
     html_code = requests.get('https://www.kinopoisk.ru/' + film_url, headers=headers).text
-    print(html_code)
+    # print(html_code)
 
     info_about_film = []
-
-    info_about_film.append(2 if html_code.find('"https://www.kinopoisk.ru/series') > -1 else 1)     # сериал или нет
-
-    type_ = info_about_film[0]
 
     info_about_film.append(get_title(html_code))                                                    # название
 
     info_about_film.append(get_description(html_code))                                              # описание
-    print(info_about_film)
+
+    info_about_film.append(get_genres(html_code))                                                   # жанры
+
+    info_about_film.append(get_year(html_code))                                                     # год
+
+    info_about_film.append(get_rating(html_code))                                                   # рейтинг
+
+    info_about_film.append(get_director(html_code))                                                 # режисер
+
+    info_about_film.append(get_slug(html_code))                                                     # слоган
+
+    info_about_film.append(get_poster(html_code))                                                   # постер
+
+    info_about_film.append(2 if html_code.find('"https://www.kinopoisk.ru/series') > -1 else 1)     # сериал или нет
+
+    info_about_film.append(film_url[film_url.find('/') + 1:-1])
+
+    util.insert_new_movie(info_about_film)
+    print('Фильм - ', film_url[5:-1], ' добавлен в БД')
 
 
 
@@ -70,12 +147,19 @@ def read_one_film(film_url):
 def main():
     number_page = 1
     while number_page <= 400:
+        print('Начинаю парсить страницу - ', number_page)
         get_page_with_films(number_page)
+        print('Закончил парсить страницу - ', number_page)
+        break
 
 
 if __name__ == '__main__':
+    pass
     # main()
     # read_one_film('series/464963/')
-    read_one_film('film/435/')
+    # read_one_film('series/77044/')
+    # read_one_film('film/435/')
     # read_one_film('film/1003587/')
     # read_one_film('film/1048766/')
+    # read_one_film('film/81542/')
+    # read_one_film('film/1043713/')
--- util.py
+++ util.py
@@ -2,4 +2,71 @@ import pymysql
 
 
 connect = pymysql.Connect(host='localhost', user='root', password='', db='kinopoisk')
-connect.cursor()
+cursor = connect.cursor()
+
+
+def insert_categories(categories, id_of_movies):
+    """
+        Добавление категорий в БД, тех которых не хватает, и сразу связывание их с фильмом.
+    :param categories:      категории
+    :param id_of_movies:    айди добавленного фильма
+    :return:
+    """
+    cursor.execute('INSERT IGNORE INTO categories (title_of_category) VALUES ("' + '"), ("'.join(categories) + '")')
+    connect.commit()
+
+    cursor.execute('SELECT id '
+                   'FROM categories '
+                   'WHERE title_of_category IN ("' + '", "'.join(categories) + '")')
+    ids_of_categories = tuple(str(id_of_movies) + ', ' + str(row[0]) for row in cursor.fetchall())
+    cursor.execute('INSERT INTO mov_with_cat (id_of_mov, id_of_cat) VALUES (' + '), ('.join(ids_of_categories) + ')')
+    connect.commit()
+
+
+def insert_year(year):
+    cursor.execute(f'INSERT IGNORE INTO years (title_of_year) VALUES ({year})')
+    connect.commit()
+
+    cursor.execute('SELECT id '
+                   'FROM years '
+                   f'WHERE title_of_year = {year}')
+    return cursor.fetchall()[0][0]
+
+
+def insert_directors(directors, id_of_movies):
+    """
+        Добавление категорий в БД, тех которых не хватает, и сразу связывание их с фильмом.
+    :param directors:       режисеры
+    :param id_of_movies:    айди добавленного фильма
+    :return:
+    """
+    cursor.execute('INSERT IGNORE INTO directors (title_of_director) VALUES ("' + '"), ("'.join(directors) + '")')
+    connect.commit()
+
+    cursor.execute('SELECT id '
+                   'FROM directors '
+                   'WHERE title_of_director IN ("' + '", "'.join(directors) + '")')
+    ids_of_directors = tuple(str(id_of_movies) + ', ' + str(row[0]) for row in cursor.fetchall())
+    cursor.execute('INSERT INTO mov_with_dir (id_of_mov, id_of_dir) VALUES (' + '), ('.join(ids_of_directors) + ')')
+    connect.commit()
+
+
+def insert_new_movie(data):
+    """
+        Добавление фильма
+    :param data:    инфа о фильма
+    :return:
+    """
+    id_of_year = insert_year(data[3])
+    cursor.execute('INSERT INTO movies (movie_name, `description`, id_of_year, rating, slug, poster, movie_or_serial, kinopoisk_id) '
+                   'VALUES ( "{}", "{}", {}, "{}", "{}", "{}", "{}", {})'.format(data[0], data[1], str(id_of_year), data[4], data[6], data[7], str(data[8]), data[9]))
+    connect.commit()
+
+    cursor.execute('SELECT MAX(id) FROM movies')
+    id_of_movie = cursor.fetchall()[0][0]
+    insert_categories(data[2], id_of_movie)
+    insert_directors(data[5], id_of_movie)
+
+
+if __name__ == '__main__':
+    pass
